5e8f8d15abc7deacaa34a27a88a731561cae1c34
Samuel Belko <samuelbelko@protonmail.com>
2023-05-09 21:41:57 +0200

    call deploydocs in docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,7 @@ makedocs(sitename = "BayesianOptimization",
          format = Documenter.HTML(),
          modules = [BayesianOptimization])
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+
+deploydocs(
+    repo = "github.com/jbrea/BayesianOptimization.jl.git",
+)
